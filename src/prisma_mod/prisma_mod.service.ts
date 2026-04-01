f1c7a45a603ff03e03c7227830f59b6ee833f911
Anand Kumar <rishavgupta163@gmail.com>
2026-04-01 16:35:48 +0530

    feat: add Prisma adapter for PostgreSQL and update dependencies

--- src/prisma_mod/prisma_mod.service.ts
+++ src/prisma_mod/prisma_mod.service.ts
@@ -6,12 +6,19 @@ import {
   type OnModuleInit,
 } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaModService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    super({ adapter });
+  }
   async onModuleInit() {
     await this.$connect();
   }
